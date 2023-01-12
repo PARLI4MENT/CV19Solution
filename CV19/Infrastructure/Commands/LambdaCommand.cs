53962bb6a112467812613700590730c6a1b15d1b
Administrator <Administrator@DESKTOP>
2023-01-12 18:20:37 +1000

    Главный класс команды нашего приложения

---
+++ CV19/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CV19.Infrastructure.Commands.Base;
+
+namespace CV19.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+        public override bool CanExecute(object parametr) => _CanExecute?.Invoke(parametr) ?? true;
+        public override void Execute(object parametr) => _Execute(parametr);
+    }
+}
