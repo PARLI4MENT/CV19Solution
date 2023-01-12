710bfc17082c4f9f0bc7623133a57aa4c1762e8e
Administrator <Administrator@DESKTOP>
2023-01-12 12:19:55 +1000

    Add project files.

---
+++ CV19CMD/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace std
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TEST");
+            Console.ReadKey();
+        }
+    }
+
+}
