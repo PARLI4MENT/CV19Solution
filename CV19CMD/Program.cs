5cc799be1fda4b917e260c7a584731e49d682ea0
Administrator <Administrator@DESKTOP>
2023-01-19 13:33:49 +1000

    Запрос к CSV

--- CV19CMD/Program.cs
+++ CV19CMD/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Net;
 
 namespace std
 {
     class Program
     {
+        private protected const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
         static void Main(string[] args)
         {
+            HttpClient client = new HttpClient();
+            var response = client.GetAsync(data_url).Result;
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+
             Console.WriteLine("TEST");
             Console.ReadKey();
         }
